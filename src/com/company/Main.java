8445b9054a3d8d9730e4e61ccf28e07c8f762a95
dleon <dleon@students.mchenry.edu>
2019-02-11 21:10:45 -0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String[] supplies = {"Buns", "Hotdogs", "Relish", "Onions", "Tomatoes", "Sport Peppers", "Pickles", "Mustard",
+            "Ketchup", "Fries", "Soda", "Paper Boats"};
+        for (String items : supplies) {
+            System.out.println(items);
+        }
+
+    }
+}
